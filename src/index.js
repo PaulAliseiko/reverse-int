44264201664cef6dca50bdd47ab6afe0ad46546c
PaulAliseiko <87947387+PaulAliseiko@users.noreply.github.com>
2022-01-17 23:49:29 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
-}
+    let strNumber = String(Math.abs(n));
+    let result = '';
+    for (let i = 0; i < strNumber.length; i++) {
+        result = strNumber[i] + result;
+    }
+    return Number(result);
+};
+
